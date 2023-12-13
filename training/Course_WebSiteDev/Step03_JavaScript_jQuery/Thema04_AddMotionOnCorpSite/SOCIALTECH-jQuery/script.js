5c2a77b2e914649065275a74017dc3e368ec196b
Taisuke-Ariyoshi. <t-ariyoshi@nifty.com>
2023-12-13 20:07:55 +0900

    「jQueryでコーポレートサイトに動きをつけよう」完了のcommit

--- training/Course_WebSiteDev/Step03_JavaScript_jQuery/Thema04_AddMotionOnCorpSite/SOCIALTECH-jQuery/script.js
+++ training/Course_WebSiteDev/Step03_JavaScript_jQuery/Thema04_AddMotionOnCorpSite/SOCIALTECH-jQuery/script.js
@@ -31,4 +31,153 @@ $(function() {
     arrows: false,
   });
 
+  // AjaxでSTATIC FORMSにデータを送信
+  $('#submit').on( 'click', function(event) {
+
+    // formタグによる送信を拒否
+    event.preventDefault();
+
+    // 入力チェックをした結果、エラーがあるかないかの判定
+    let result = inputCheck();
+
+    // エラー判定とメッセージを取得
+    let error = result.error;
+    let message = result.message;
+
+    //エラーが無かったらフォームを送信する
+    if ( error == false ) {
+      console.log('問題なし！');
+      // Ajaxでformを送信する
+      
+      $.ajax({
+        url: 'https://api.staticforms.xyz/submit',
+        type: 'POST',
+        dataType: 'json',
+        data: $('#form').serialize(),
+        success: function (result) {
+          alert('お問い合わせを送信しました。')
+        },
+        error: function (xhr, resp, text) {
+          alert('お問い合わせを送信できませんでした。')
+        }
+      })
+    
+    } else {
+      // エラーメッセージを表示する
+      alert( message );
+    }
+
+  });
+
+  // フォーカスが外れたとき（blur）にフォームの入力チェックをする
+  $('#name').blur( function() {
+    inputCheck();
+  });
+  $('#furigana').blur( function() {
+    inputCheck();
+  });
+  $('#email').blur( function() {
+    inputCheck();
+  });
+  $('#tel').blur( function() {
+    inputCheck();
+  });
+  $('#message').blur( function() {
+    inputCheck();
+  });
+  $('#agree').blur( function() {
+    inputCheck();
+  });
+
+  // お問い合わせフォームの入力チェック
+  function inputCheck() {
+
+    // エラーチェックの結果
+    let result = false;
+
+    // エラーメッセージのテキスト
+    let message = "";
+
+    // エラーが無ければfalse、あればtrue
+    let error = false;
+
+    // お名前のチェック
+    if ( $('#name').val() == "" ) {
+      // エラーあり
+      error = true;
+      $('#name').css( 'background-color', '#f79999' );
+      message += "お名前を入力してください。\n";
+    } else {
+      // エラーなし
+      $('#name').css( 'background-color', '#fafafa' );
+    }
+
+    // フリガナのチェック
+    if ( $('#furigana').val() == "" ) {
+      // エラーあり
+      error = true;
+      $('#furigana').css( 'background-color', '#f79999' );
+      message += "フリガナを入力してください。\n";
+    } else {
+      // エラーなし
+      $('#furigana').css( 'background-color', '#fafafa' );
+    }
+
+    // お問い合わせのチェック
+    if ( $('#message').val() == "" ) {
+      // エラーあり
+      error = true;
+      $('#message').css( 'background-color', '#f79999' );
+      message += "お問い合わせを入力してください。\n";
+    } else {
+      // エラーなし
+      $('#message').css( 'background-color', '#fafafa' );
+    }
+
+    // メールアドレスのチェック
+    if ( $('#email').val() == '' || $('#email').val().indexOf('@') == -1 || $('#email').val().indexOf('.') == -1 ) {
+      // エラーあり
+      $('#email').css( 'background-color', '#f79999' );
+      error = true;
+      message += "メールが未記入。または「@」や「.」が含まれていません。/n";
+    } else {
+      // エラーなし
+      $('#email').css( 'background-color', '#fafafa' );
+    }
+
+    // 電話番号のチェック
+    if ( $('#tel').val() == '' || $('#tel').val().indexOf('-') == -1 ) {
+      // エラーあり
+      $('#tel').css( 'background-color', '#fa9999' );
+      error = true;
+      message += "電話番号に「-」が含まれていません。\n";
+    } else {
+      // エラーなし
+      $('#tel').css( 'background-color', '#fafafa' );
+    }
+
+    // 個人情報のチェックボックスのチェック
+    if ( $('#agree').prop('checked') == false ) {
+      error = true;
+      message += "個人情報の取り扱いについてご同意いただける場合は、チェックボックスにチェックをしてください。\n";
+    }
+
+    // エラーの有無で送信ボタンの切り替え
+    if ( error == true ) {
+      $('#submit').attr( 'src', 'images/button-submit.png' );
+    } else {
+      $('#submit').attr( 'src', 'images/button-submit-blue.png');
+    }
+
+    // オブジェクトでエラー判定とメッセージを返す
+    result = {
+      error: error,
+      message: message
+    }
+
+    // 戻り値としてエラーであるかどうかを返す
+    return result;
+
+  }
+
 });
